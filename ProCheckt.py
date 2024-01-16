ffbb1b77312d9c4ec2d4e97acb41eb71f16294c8
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-01-15 17:16:58 -0800

    Add files via upload

---
+++ ProCheckt.py
@@ -0,0 +1,33 @@
+import sys
+
+Check = sys.argv[1]
+firstcharmap = "~/sure\ buddy/test1.txt"
+secondcharmap = "~/sure\ buddy/test.txt"
+check = sys.argv[2]
+tick = 0
+model = []
+with open(str(check)+str(Check)+"charcheck.txt", "a") as c:
+    with open(firstcharmap, "r") as charmap:
+        for line in charmap:
+            for char in line:
+                if (tick < check):
+                    model.append(char)
+                    tick += 1
+                else:
+                    model.append(char)
+                    x = lambda x: c.write(os.popen(check+" "+x).read())
+                    (list(map(x, model)))
+                    model = []
+                    tick = 0
+    tick = 0
+    with open(secondcharmap, "r") as charmap2:
+        for lines in charmap2:
+            for chars in line:
+                if (tick < check):
+                    model.append(chars)
+                    tick += 1
+                else:
+                    model.append(char)
+                    x = lambda x: c.write(os.popen(check+" "+x).read())
+                    model = []
+                    tick = 0
