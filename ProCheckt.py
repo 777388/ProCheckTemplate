c0cf588b292a8f50a8f3cc6df8a0144c56886255
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-01-15 19:21:49 -0800

    Update ProCheckt.py

--- ProCheckt.py
+++ ProCheckt.py
@@ -6,12 +6,15 @@ secondcharmap = "test.txt"
 check = int(sys.argv[2])
 tick = 0
 model = []
-
+errors = []
+output = []
 def test(x):
     try:
-        os.popen(checks+" "+x).read()
+        output.append(os.popen(checks+" "+x).read()+"\r")
+        return os.popen(checks+" "+x)
     except Exception as e:
-        print(x+" has an error:"+str(e))
+        errors.append(x+" has an error:"+str(e)+"\r")
+        pass
 with open(str(check)+"_"+str(checks)+"_procheckt.txt", "a") as c:
     with open(firstcharmap, "r") as charmap:
         for line in charmap:
@@ -21,7 +24,7 @@ with open(str(check)+"_"+str(checks)+"_procheckt.txt", "a") as c:
                     tick += 1
                 else:
                     model.append(char)
-                    x = lambda x: c.write(str(test(x)))
+                    x = lambda x: test(x)
                     (list(map(x, model)))
                     model = []
                     tick = 0
@@ -34,7 +37,13 @@ with open(str(check)+"_"+str(checks)+"_procheckt.txt", "a") as c:
                     tick += 1
                 else:
                     model.append(char)
-                    x = lambda x: c.write(str(test(x)))
+                    x = lambda x: test(x)
                     (list(map(x, model)))
                     model = []
                     tick = 0
+
+    print(errors)
+    print("Would you like to save the output to a report log named "+str(check)+"_"+str(checks)+"_procheckt.txt? y or n")
+    tellme = input()
+    if(tellme == "y"):
+        c.write(output)
